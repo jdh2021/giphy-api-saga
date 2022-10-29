b5d01c15e03d9eedc68b7906f74913f2a93abf73
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-10-29 10:37:02 -0500

    created initial placeholder components for Search and Favorites. updated client-side routing in App.js.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { HashRouter as Router, Route } from "react-router-dom";
+import Search from '../Search/Search';
+import Favorites from '../Favorites/Favorites';
+
 
 function App(props) {
   return (
-    <div>
-      <h1>Giphy Search!</h1>
-    </div>
+    <Router>
+      <div>
+        <h1>Giphy Search!</h1>
+        <Route exact path='/'>
+          <Search />
+        </Route>
+        <Route exact path='/favorites'>
+          <Favorites />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
