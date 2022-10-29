7362934966ff6be24459564fa4708914fa6e8de9
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-10-29 12:11:05 -0500

    created table to display searched GIFs. useSelector to retrieve search results from store and loop over them.

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -8,19 +8,48 @@ function Search() {
     // local state for changeable values of input fields
     const [searchTerm, setSearchTerm] = useState('');
 
+    //useSelector to retrieve search results from store
+    const searchResults = useSelector(store => store.results);
+
     const submitSearch = () => {
         console.log('searchTerm is:', searchTerm);
-        dispatch({ type: 'FETCH_GIFS', payload: searchTerm})
+        dispatch({ type: 'FETCH_GIFS', payload: searchTerm })
     }
 
-    return(
+    // dispatch 'FETCH_CATEGORIES' when page loads
+    useEffect(() => {
+        dispatch({ type: "FETCH_CATEGORIES" })
+    }, []);
+
+    return (
         <div>
-        <div>
-            <h3>Search</h3>
-            <input type="text" placeholder="Search Term" value={searchTerm} onChange={(event) =>setSearchTerm(event.target.value)} />
-            <button onClick={submitSearch}>Submit</button>
-        </div>
-        <h3>Search Results</h3>
+            <div>
+                <h3>Search</h3>
+                <input type="text" placeholder="Search Term" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+                <button onClick={submitSearch}>Submit</button>
+            </div>
+            <table>
+                <thead><tr><td><h3>Search Results</h3></td></tr></thead>
+                <tbody>
+                    {searchResults.map(gif => {
+                        console.log(gif.images.fixed_height_small.url);
+                        return ( 
+                            <tr>
+                                <td>
+                                    <img src={gif.images.fixed_height_small.url} width="250" height="250" />
+                                    <br />
+                                    <label for="categories">Choose a category: </label>
+                                    <select name="categories" id="categories">
+                                        {/* loop over categories */}
+                                    </select>
+                                    <br />
+                                    <button>Make Favorite</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     )
 }
