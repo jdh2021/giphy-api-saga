4bc79e5a1a47f81d4df94ac4369ce635efa8552e
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-11-02 09:50:03 -0500

    useState to set favorite status of Search Result. added for of loop to loop over favorites array and check if Search Result id matches id of favorited GIF. updated Favorite button to render as disabled when GIF is an existing favorite.

--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -3,7 +3,19 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function SearchResult({ gif, categories }) {
     const dispatch = useDispatch();
+    // useSelector to retrieve favorites from store
+    const favorites = useSelector(store => store.favorites);
+
+    // local state to set default value of category
     const [categoryId, setCategoryId] = useState(categories[0].id);
+    
+    // local state for favorite status of Search Result
+    const [favoriteStatus, setFavoriteStatus] = useState(false);
+
+    // calls checkFavoriteStatus when length of favorites array changes
+    useEffect(() => {
+        checkFavoriteStatus();
+    }, [favorites.length]);
 
     const submitFavorite = (categoryId, giphyId, giphyUrl) => {
         console.log('selected category is:', categoryId);
@@ -13,7 +25,18 @@ function SearchResult({ gif, categories }) {
             return;
         }
         else {
-            dispatch({ type: 'POST_FAVORITE', payload: { giphy_id: giphyId, category_id: categoryId, giphy_url: giphyUrl } })
+            dispatch({ type: 'POST_FAVORITE', payload: { giphy_id: giphyId, category_id: categoryId, giphy_url: giphyUrl }})
+        }
+    }
+
+    // loops over favorites array from store. if id of Search Result matches id of existing favorite, sets favoriteStatus to true
+    // allows for conditional rendering of Favorite button
+    const checkFavoriteStatus = () => {
+        for (let favorite of favorites) {
+            if (favorite.giphy_id === gif.id) {
+                setFavoriteStatus(true);
+                return;
+            }
         }
     }
 
@@ -31,7 +54,10 @@ function SearchResult({ gif, categories }) {
                     })}
                 </select>
                 <br />
-                <button onClick={() => submitFavorite(categoryId, gif.id, gif.images.fixed_height_small.url)}>Favorite</button>
+                {favoriteStatus ?
+                    <button disabled>Already Favorited</button> :
+                    <button onClick={() => submitFavorite(categoryId, gif.id, gif.images.fixed_height_small.url)}>Favorite</button>
+                }
             </td>
         </tr>
     )
